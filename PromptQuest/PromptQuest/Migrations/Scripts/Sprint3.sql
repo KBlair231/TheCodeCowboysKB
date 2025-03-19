abaf4807e7626ae97986b800b4cae6d6c1c59e22
BenRoth0 <br.roths@gmail.com>
2025-03-19 02:20:15 -0700

    Added migration script for Sprint 3

---
+++ PromptQuest/PromptQuest/Migrations/Scripts/Sprint3.sql
@@ -0,0 +1,13 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Players] ADD [Class] nvarchar(max) NOT NULL DEFAULT N'';
+
+ALTER TABLE [GameStates] ADD [IsLocationComplete] bit NOT NULL DEFAULT CAST(0 AS bit);
+
+ALTER TABLE [GameStates] ADD [PlayerLocation] int NOT NULL DEFAULT 0;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250319090506_Sprint3', N'9.0.2');
+
+COMMIT;
+GO
+
